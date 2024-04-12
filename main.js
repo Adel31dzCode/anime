ed1e2088edb47cf4504a7d5ad6ed84dd913ec2fc
Adel31dzCode <132948186+Adel31dzCode@users.noreply.github.com>
2024-04-12 20:27:11 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -29,3 +29,93 @@ document.addEventListener('click', function(event) {
         });
     }
 });
+
+
+
+
+
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    autoplay: {
+        delay: 3000,
+      },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+
+
+  
+
+var swiper1 = new Swiper(".myswiper", {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 7,
+    spaceBetween: 20,
+    freeMode: true,
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        800: {
+            slidesPerView: 3,
+            spaceBetween: 30
+          },
+        1000: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+          },
+          1200: {
+            slidesPerView: 4,
+            spaceBetween: 45,
+          },
+        1300: {
+          slidesPerView: 5,
+          spaceBetween: 0,
+        },
+        1400: {
+          slidesPerView: 5,
+          spaceBetween: 20,
+        },
+        1600: {
+            slidesPerView: 7,
+            spaceBetween: 0,
+          }
+      },
+
+
+
+    autoplay: {
+        delay: 7000,
+        reverseDirection: true,
+    },
+    navigation: {
+        nextEl: '.nxt',
+        prevEl: '.prv',
+    },
+});
+
+
+const btn_srch = document.getElementById("focus_link");
+const inp_search = document.getElementById("search");
+
+btn_srch.addEventListener('click', ()=> {
+    inp_search.focus();
+})
